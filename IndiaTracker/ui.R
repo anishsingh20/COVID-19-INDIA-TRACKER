e65040bd15c38c49418e44d86fb20ab3cb7ae272
anishsingh20 <loveeanish@gmail.com>
2020-03-27 17:00:43 +0530

    imported the data in R

--- IndiaTracker/ui.R
+++ IndiaTracker/ui.R
@@ -4,14 +4,27 @@
 #complete cases dataset
 url <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc_2y5N0I67wDU38DjDh35IZSIS30rQf7_NYZhtYYGU1jJYT6_kDx4YpF-qw0LSlGsBYP8pqM_a1Pd/pubhtml#"
 
-#Time series URL
-url1<- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc_2y5N0I67wDU38DjDh35IZSIS30rQf7_NYZhtYYGU1jJYT6_kDx4YpF-qw0LSlGsBYP8pqM_a1Pd/pubhtml#"
 
-#Daily changes in COVID-19 cases
-url2 <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc_2y5N0I67wDU38DjDh35IZSIS30rQf7_NYZhtYYGU1jJYT6_kDx4YpF-qw0LSlGsBYP8pqM_a1Pd/pubhtml#"
-
-#statewise records
-url3<- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSc_2y5N0I67wDU38DjDh35IZSIS30rQf7_NYZhtYYGU1jJYT6_kDx4YpF-qw0LSlGsBYP8pqM_a1Pd/pubhtml#"
+require(rvest)
 
 
+#reading the table from the URL using rvest package
+h1<- read_html(url)
+html_text(h1)
+
+
+
+
+tab <- tab %>% setNames(c("S.No", "State", "Confirmed", "Recovered","Deaths","Active",
+                          "Last_Updated_Time")) 
+
+#removing the NA column
+tab <- h1 %>% html_nodes("table")
+tab <- tab[[1]] %>% html_table
+tab <- na.omit(tab)
+
+
+tab <- tab[colSums(!is.na(tab)) > 0]
+#removing the NA rows
+tab <- na.omit(tab)
 
